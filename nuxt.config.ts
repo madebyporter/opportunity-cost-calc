6b89ef77bd75d567f368018bb569d4cb09a40ca9
madebyporter <general@madebyporter.com>
2024-02-21 23:19:55 -0500

    added gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/guide/directory-structure/nuxt.config
 export default defineNuxtConfig({
   css: ['~/assets/css/output.css'],
-  telemetry: false
+  telemetry: false,
+  modules: ['nuxt-gtag'],
+  gtag: {
+    id: 'G-RDQ1WD4WB5'
+  }
 })
